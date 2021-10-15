a2f7c6714f16064bc37674bf6a23322f4f93e44d
Matthew Knowles <mattknowles314@gmail.com>
2021-10-15 14:29:14 +0100

    new figure

--- code/bayesAnalysis.R
+++ code/bayesAnalysis.R
@@ -1,1 +1,16 @@
 df <- read.csv("/Volumes/MattData/CricData/firstinns.csv", header = T)
+df[is.na(df)]<-0
+fowDF <- df[,52:61]
+fowAvgs <- c()
+
+for(j in 1:10){
+    fowAvgs[j] <- mean(fowDF[,j])
+}
+
+plot(fowAvgs,pch=19, xlab = "Fall of Wicket", ylab="Average Runs")
+lines(fowAvgs)
+
+mu <- mean(fowAvgs)
+sigma2 <- var(fowAvgs)
+
+ks.test(fowAvgs, pnorm)
