9914779d85bacd0a2f759af2d75f109a4d513648
Matthew Knowles <mattknowles314@gmail.com>
2021-10-22 08:17:02 +0100

    Extended data collection

--- code/bayesAnalysis.R
+++ code/bayesAnalysis.R
@@ -7,10 +7,18 @@ for(j in 1:10){
     fowAvgs[j] <- mean(fowDF[,j])
 }
 
-plot(fowAvgs,pch=19, xlab = "Fall of Wicket", ylab="Average Runs")
-lines(fowAvgs)
+plot(1:length(fowAvgs),fowAvgs,pch=19, xlab = "Fall of Wicket", ylab="Average Runs", main="Average runs for each fall of wicket", type="b")
+abline(h=mu, col="purple")
+abline(h=(mu+sigma), col="red", title="+1 SD")
+abline(h=(mu-sigma), col="red", label="-1 SD")
+text(2,166,"+1SD")
+text(2,125, "Mean")
+text(2,55, "-1SD")
 
 mu <- mean(fowAvgs)
 sigma2 <- var(fowAvgs)
 
 ks.test(fowAvgs, pnorm)
+
+
+
