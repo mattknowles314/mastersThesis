68161083a3aea2f50cf82b7143d02d26f3b733d6
Matthew Knowles <mattknowles314@gmail.com>
2021-10-30 01:20:00 +0100

    Randomly initialises weight and biases, also calculates costs for each example

--- code/net.py
+++ code/net.py
@@ -1,6 +1,8 @@
 import math
 import csv
 import pandas as pd
+import random
+import numpy as np
 
 #Organising data
 
@@ -26,9 +28,7 @@ def next_layer(W, a, b):
     Takes in a weight matrix W, the current layer of values, a, and the vector of biases. Outputs the vector of 
     values corresponding to the next layer in the network.
     '''
-    if not is_Valid(W,len(a),len(b)):
-        return False
-    else:
+    if is_Valid(W,len(a),len(b)):
         A = []
         for row in W:
             sum = 0
@@ -38,12 +38,32 @@ def next_layer(W, a, b):
         for a in A:
             A[A.index(a)]= act(a + b[A.index(a)])
         return A
+    else:
+        return False
 
+#Initialse random weights and biases
+W1 = [[random.randint(-300,300) for x in range(0,50)] for y in range(0,25)]
+b1 = [1 for x in range(0,25)]
+W2 = [[random.randint(-300,300) for x in range(0,25)] for y in range(0,10)]
+b2 = [1 for x in range(0,10)]
+W3 = [[random.randint(-10,10) for x in range(0,10)] for y in range(0,3)]
+b3 = [1 for x in range(0,3)]
+W4 = [random.randint(-300,300) for y in range(0,3)]
+b4 = [1 for x in range(0,3)]
 
+costs = []
+def get_costs(X,y):
+    a0 = list((rrmat.iloc[0])[0:-1])
+    a1 = next_layer(W1,a0,b1)
+    a2 = next_layer(W2,a1,b2)
+    a3 = next_layer(W3,a2,b3)
+    a4 = np.dot(W4,a3)
+    costs.append((a4-y)**2)
 
+i = 0
+while i <= 1435:
+    get_costs(list((rrmat.iloc[i])[0:-1]),Y[i])
+    i+=1
 
-W = [[1,2,3],[4,2,6]]
-a = [6,7,9]
-b = [1,1]
-
-print(next_layer(W,a,b))
+print(costs)
+print(len(costs))
