7845ca3ec29afc247ef69feb9e3a20c3a1a0fb0f
Matthew Knowles <mattknowles314@gmail.com>
2021-10-30 13:10:22 +0100

    Implemented huber loss function, but doesn't do anything yet

--- code/net.py
+++ code/net.py
@@ -23,6 +23,12 @@ def is_Valid(M,n,k):
                 return False
         return True
 
+def huber(y1,y,delta=1.0):
+    if (np.abs(y-y1)) <= delta:
+        return 0.5*(y-y1)**2
+    else:
+        return delta*(np.abs(y-y1)-0.5*delta)
+
 def next_layer(W, a, b):
     '''
     Takes in a weight matrix W, the current layer of values, a, and the vector of biases. Outputs the vector of 
@@ -42,13 +48,13 @@ def next_layer(W, a, b):
         return False
 
 #Initialse random weights and biases
-W1 = [[random.randint(-300,300) for x in range(0,50)] for y in range(0,25)]
+W1 = [[random.randint(-1000,1000) for x in range(0,50)] for y in range(0,25)]
 b1 = [1 for x in range(0,25)]
-W2 = [[random.randint(-300,300) for x in range(0,25)] for y in range(0,10)]
+W2 = [[random.randint(-1000,1000) for x in range(0,25)] for y in range(0,10)]
 b2 = [1 for x in range(0,10)]
-W3 = [[random.randint(-10,10) for x in range(0,10)] for y in range(0,3)]
+W3 = [[random.randint(-1000,1000) for x in range(0,10)] for y in range(0,3)]
 b3 = [1 for x in range(0,3)]
-W4 = [random.randint(-300,300) for y in range(0,3)]
+W4 = [random.randint(-1000,1000) for y in range(0,3)]
 b4 = [1 for x in range(0,3)]
 
 costs = []
@@ -65,5 +71,4 @@ while i <= 1435:
     get_costs(list((rrmat.iloc[i])[0:-1]),Y[i])
     i+=1
 
-print(costs)
-print(len(costs))
+print("Cost: ",np.average(costs))
