0930f45f35d69c305434a4e76e823cf6ad4d04aa
Matthew Knowles <mattknowles314@gmail.com>
2021-12-13 20:19:03 +0000

    Changes

--- code/scoreNet.py
+++ code/scoreNet.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
-from sklearn.neural_network import MLPRegressor
-from sklearn.neural_network import MLPClassifier
+import tensorflow as tf
 
 rrmatTrain = pd.read_csv("rrmatTrain.csv",header=None)
 rrmatTest = pd.read_csv("rrmatTest.csv",header=None)
@@ -12,14 +11,18 @@ y = rrmatTrain[50]
 del rrmatTrain[50]
 X = rrmatTrain
 
+print(X.shape[1])
 
-regr = MLPRegressor(solver="sgd", alpha=0.001, hidden_layer_sizes=(25,10,3), random_state=1, activation='logistic',max_iter=1000)
-regr.fit(X,y)
+tf.keras.backend.clear_session()
+tf.random.set_seed(60)
 
-clf = MLPClassifier(solver="adam", alpha=0.001, hidden_layer_sizes=(25,10,3), random_state=1, activation='logistic',max_iter=1000)
-clf.fit(X,y)
+model=keras.models.Sequential([
+    keras.layers.Dense(50,input_dim=X.shape[1], activation="relu"),
+    keras.layers.Dense(50,input_dim=X.shape[1], activation="relu"),
+    keras.layers.Dense(units=25,activation="relu"),
+    keras.layers.Dense(units=25,activation="relu"),
+    keras.layers.Dense(units=3,activation="relu"),
+    keras.layers.Dense(units=1),
+],name="Initial Model")
 
-z = rrmatTest[50]
-del rrmatTest[50]
-D = rrmatTest
-
+model.summary()
